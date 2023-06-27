fa040b5cc900e333a934a479d52f16d98fdfa3ca
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2023-06-27 18:00:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,8 +97,8 @@ with col2:
         b = st.write("Enter your email to get TAM value")
         with st.form("email_form"):
             email = st.text_input(" Enter the email : ")
-            submit_button = st.form_submit_button("Submit")
-        if submit_button:    
+            b2 = st.form_submit_button("Submit your email id")
+        if b2:    
             sheet = client.open('form_to_sheet').sheet1
             tam_value = number_of_people*number
             # b = st.button('Click this button')
